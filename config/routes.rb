04c901502715328c199a0218458a815a28d4c198
YourName <you@example.com>
2020-02-20 17:58:32 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+  resources :homes,only: [:inedx]
+  resources :cafes,only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
