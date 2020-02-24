6c09c4eb6c64d898c0cece6f332a974581fd9a35
YourName <you@example.com>
2020-02-24 13:28:14 +0900

    add

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -6,4 +6,12 @@ $(function() {
     autoplaySpeed: 4000,
     arrows: false,
   });
+
+  $('.top__photo').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  });
 });
